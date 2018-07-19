4c167f7eb8d6ff4985923dd298d85677e1d9d0f3
JoonHo Park <pjhjohn@gmail.com>
2018-07-19 20:30:40 +0900

    Display Input through Shared ViewModel
    Changes
    - Enable databinding with `gradle.properties` updates (also for androidx)
    - XML updates for databinding
      - Click binding for panel buttons
      - Text binding for display text
    - Share ViewModel using activity tag
    - View inflation with DataBindingUtil, attaching lifecycle owner

--- app/src/main/java/io/pjhjohn/calculator/view/active/ActiveCalculatorPanelFragment.kt
+++ app/src/main/java/io/pjhjohn/calculator/view/active/ActiveCalculatorPanelFragment.kt
@@ -1,12 +1,14 @@
 package io.pjhjohn.calculator.view.active
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import io.pjhjohn.calculator.R
+import io.pjhjohn.calculator.databinding.FragmentActiveCalculatorPanelBinding
 
 class ActiveCalculatorPanelFragment : Fragment() {
 
@@ -14,17 +16,19 @@ class ActiveCalculatorPanelFragment : Fragment() {
         fun newInstance() = ActiveCalculatorPanelFragment()
     }
 
+    private lateinit var binding: FragmentActiveCalculatorPanelBinding
     private lateinit var viewModel: ActiveCalculatorViewModel
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_active_calculator_panel, container, false)
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ActiveCalculatorViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProviders.of(requireActivity()).get(ActiveCalculatorViewModel::class.java)
+            .also { binding.vm = it }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_active_calculator_panel, container, false)
+        binding.setLifecycleOwner(this)
+        return binding.root
     }
 
 }
--- app/src/main/java/io/pjhjohn/calculator/view/active/ActiveCalculatorViewModel.kt
+++ app/src/main/java/io/pjhjohn/calculator/view/active/ActiveCalculatorViewModel.kt
@@ -1,7 +1,14 @@
 package io.pjhjohn.calculator.view.active
 
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 
 class ActiveCalculatorViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val evaluationResult: ObservableField<String> = ObservableField("initial")
+
+    fun input(value: String) {
+        evaluationResult.set(value)
+    }
+
 }
